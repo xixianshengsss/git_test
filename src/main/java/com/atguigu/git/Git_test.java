5c976390dce49934b4b9acc0178c920d353234b3
xxs <1991392207@qq.com>
2023-01-17 13:25:29 +0800

    second tow commit

--- src/main/java/com/atguigu/git/Git_test.java
+++ src/main/java/com/atguigu/git/Git_test.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class Git_test {
     public static void main(String[] args) {
         System.out.println("first one");
+        System.out.println("second tow commit");
     }
 }
